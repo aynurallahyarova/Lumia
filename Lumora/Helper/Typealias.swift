d16510551c3bfc0e26ed9fbf9251de982a0b49d2
Aynur Allahyarova <aynurallahyarova5@gmail.com>
2026-02-22 23:16:31 +0400

    typealias and imageView + extension

---
+++ Lumora/Helper/Typealias.swift
@@ -0,0 +1,10 @@
+//
+//  Typealias.swift
+//  Lumora
+//
+//  Created by Aynur on 22.02.26.
+//
+
+import UIKit
+
+typealias CollectionConfiguration = UICollectionViewDataSource & UICollectionViewDelegate & UICollectionViewDelegateFlowLayout
---
+++ Lumora/Helper/UIImageView+Extension.swift
@@ -0,0 +1,17 @@
+//
+//  UIImageView+Extension.swift
+//  Lumora
+//
+//  Created by Aynur on 22.02.26.
+//
+
+import UIKit
+import Kingfisher
+
+
+extension UIImageView {
+    func loadURL(data: String) {
+        let url = URL(string: <#T##String#>)
+        kf.setImage(with: url)
+    }
+}
